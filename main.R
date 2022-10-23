1ffa84c4cb207979fb03ea7011aede7104977b8f
newfrogg <85973867+newfrogg@users.noreply.github.com>
2022-10-23 21:27:54 +0700

    package data to auto-mpg.csv

--- main.R
+++ main.R
@@ -15,7 +15,33 @@ col_names <- c("mpg","cylinders","displacement",
             "horsepower","weight","acceleration",
             "modelyear","origin", "carname")
 
-dataFrame <- read.table(url, fileEncoding = "UTF-8", dec=',',
-                        col.names = col_names)
+dataFrame <- read.table(url, fileEncoding = "UTF-8", dec=",", col.names = col_names)
+
+rows = nrow(dataFrame)
+cols = ncol(dataFrame)
+
+# Change data type
+
+# for (i in 1:cols) {
+#     print(typeof(dataFrame[, i]) )
+# }
+
+dataFrame[,c(1,2,3,4,5)] <- suppressWarnings(apply(dataFrame[,c(1,2,3,4,5)], 2, as.integer))
+
+print("Our dataset has a missing property in at least 1 sample. In this case,
+      N/A appears in 5th column (horsepower)")
+
+# Package to .csv file
+
+fileName <- "./data/auto-mpg.csv"
+
+if (is.null(
+            try(
+                write.csv(dataFrame,
+                          file = fileName,
+                          row.names = TRUE,
+                          na = ""))) != FALSE)
+    print("Writing to CSV successfully.")
+
 
 
